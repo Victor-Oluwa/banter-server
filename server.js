a814ae35fbfd4a7050d014ced3b73ba77c568b9f
oluwafemi <gm.oluwafemi@gmail.com>
2024-10-20 15:01:38 +0100

    _

--- server.js
+++ server.js
@@ -11,19 +11,3 @@ app.listen(PORT, () => {
 });
 
 
-
-
-
-
-
-
-//gmoluwafemi
-//
-//DgCYfk9pLJXDxwS8
-
-//mongodb+srv://gmoluwafemi:DgCYfk9pLJXDxwS8@bantercluster.lo0rd.mongodb.net/?retryWrites=true&w=majority&appName=BanterCluster
-
-// Eminem
-// XXX tentansion
-// Tupac
-// Mbape
