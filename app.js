1c8ecd54a455faf2bc605e64db86b0ad6a01001f
oluwafemi <gm.oluwafemi@gmail.com>
2024-10-20 20:26:04 +0100

    Moved PORT

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRouter = require('./src/app/auth/auth.route');
 const noteRouter = require('./src/app/notes/route/note.route');
+const PORT = process.env.PORT || 5000;
+
 
 dotenv.config();
 
@@ -27,5 +29,11 @@ app.use(noteRouter);
 app.get('/', (req, res) => {
     res.send('API is runnning');
 });
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const app = require("./app");
+// const app = require("./app");
 
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
 
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
 
